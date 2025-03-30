5898d9ca9a4ad77f781f994d02e2b31835e0a8df
morsteinberg <mors951@gmail.com>
2025-03-30 20:00:10 +0300

    adding sxprop type

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import TopBar from './TopBar/TopBar';
 import { Box } from '@mui/material';
+import * as styles from './layout.styles';
 
 const Layout = () => {
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        <Box sx={styles.layout}>
             <TopBar />
-            <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Box sx={styles.outlet}>
                 <Outlet />
             </Box>
         </Box>
---
+++ src/layout/layout.styles.ts
@@ -0,0 +1,14 @@
+import { SxProps } from "@mui/material";
+
+export const layout: SxProps = {
+  display: "flex",
+  flexDirection: "column",
+  height: "100vh",
+};
+
+export const outlet: SxProps = {
+  flexGrow: 1,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
