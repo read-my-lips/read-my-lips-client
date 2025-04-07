87fad21d08ae299beae5a264ab360d581af0e732
morsteinberg <mors951@gmail.com>
2025-04-08 01:08:28 +0300

    styles for prediction page

--- src/pages/prediction/Prediction.tsx
+++ src/pages/prediction/Prediction.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import { useLocation } from "react-router-dom";
+import * as styles from './prediction.styles';
 
 type PredictionState = {
     prediction: string;
@@ -9,15 +10,7 @@ const Prediction = () => {
     const location = useLocation();
     const { prediction } = (location.state as PredictionState) || { prediction: "No prediction available" };
     return (
-        <Box sx={{
-            flexGrow: 1,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            gap: "2rem",
-        }}>
+        <Box sx={styles.container}>
             <Typography variant="h4">The prediction for this video is : </Typography>
             <Typography variant="h5">{prediction}</Typography>
         </Box>
